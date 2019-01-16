2962facc217f6656e94003008da7268047e1392d
David Haines <davidwhainesiii@gmail.com>
2019-01-15 20:26:21 -0600

    additional testing, twitter scrape included

--- testing.R
+++ testing.R
@@ -9,18 +9,10 @@
 # enable interactivity to show pictures associated with words in cloud based on likes/retweets
 # (size pictures based on popularity of associated tweet?)
 
-testlist <- list()
-testseq <- seq(1,10)
-
-for (i in 1:length(testseq)) {
-  append(i, testlist)
-}
-
 library(httr)
-library(rvest)
 library(tm)
-library(iterators)
 library(jsonlite)
+library(rtweet)
 library(rtweet)
 apikey <- c("jyfIazj3xBfqar7uYKV0GVSOF2rmLyGihb5N540e")
 queryseq <- seq(as.Date("2017/6/30"), as.Date("2017/12/31"), by = "day")
@@ -36,14 +28,30 @@ gets_df <- data.frame(Date=as.Date(character()),
                  Retweets=character(),
                  Likes=character(),
                  stringsAsFactors=FALSE)
-names(gets_df) <- c()
+# names(gets_df) <- c()
 for (i in seq_along(dummydates)) {
   apod <- paste0("https://api.nasa.gov/planetary/apod?date=", dummydates[i],"&api_key=jyfIazj3xBfqar7uYKV0GVSOF2rmLyGihb5N540e")
   iterget <- GET(apod)
   decode <- fromJSON(as.character(iterget), simplifyDataFrame = TRUE)
-  gets_df$Explanation <- na.omit(decode$explanation)
+  gets_df[i,'Date'] <- c(decode$date)
+  gets_df[i,'Explanation'] <- c(decode$explanation)
   #gets_df$date <- append(gets_df, decode$date)
 }
+
+
+#Create token
+token <- create_token(
+  app = "GreatestAPODs",
+  consumer_key = "japcb18LtppGNSW2yWA24C894",
+  consumer_secret = "JmOZOQFQ6jEhpzG4IHSd2Ddf8XBGMNVUxZnlJsWIoX9mAe7f9N",
+  access_token = "9551451262-wK2EmA942kxZYIwa5LMKZoQA4Xc2uyIiEwu2YXL",
+  access_secret = "9vpiSGKg1fIPQtxc5d5ESiFlZQpfbknEN1f1m2xe5byw7")
+
+# Get info about the APOD twitter account
+apodtwitter <- lookup_users("apod")
+
+#Get apod timeline info (2000 tweets)
+apodtimeline <- get_timeline(user = 8295072, n = 2000)
 
 # looper function
 loopedget <- function(date) {
